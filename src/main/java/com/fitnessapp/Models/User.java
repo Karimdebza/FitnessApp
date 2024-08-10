8e1b1069fdc8536f597451e551d929f13baaea63
karim debza <bwb-k.debza@fondespierre.com>
2024-08-10 14:47:39 +0200

    Mettre en place le model User

--- src/main/java/com/fitnessapp/Models/User.java
+++ src/main/java/com/fitnessapp/Models/User.java
@@ -13,7 +13,46 @@ public class User {
     private String username;
     private String email;
     private String password;
-    private Time create_at;
+    private String create_at;
 
     public User(){}
+
+    public  User( String username, String email, String password, String create_at){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.create_at = create_at;
+    }
+
+    public  int getId_user() {
+        return id_user;
+    }
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getCreate_at() {
+        return create_at;
+    }
+    public void setCreate_at(String create_at) {
+        this.create_at = create_at;
+    }
+
 }
